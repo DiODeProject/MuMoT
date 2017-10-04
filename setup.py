e20551b261b41b47ed07f888e6d36df917bae2e6
James Marshall <james.marshall@shef.ac.uk>
2017-10-04 15:41:16 +0100

    fixed broken setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='MuMoT',
+    description='Multiscale Modelling Tool',
+    author='James A. R. Marshall, Andreagiovanni Reina, Thomas Bose',
+    author_email='james.marshall@shef.ac.uk',
     version='0.0',
+    url='https://github.com/DiODeProject/MuMoT',
+    packages=['mumot','mumot.gen']
     install_requires=requirements)
