fa4b59a4b4b174482f50c71d881b0824a4419d63
Will Furnass <will@thearete.co.uk>
2017-11-09 14:16:34 +0000

    Tidy up imports and dependency management

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+from setuptools import setup
 
 setup(name='MuMoT',
-    description='Multiscale Modelling Tool',
-    author='James A. R. Marshall, Andreagiovanni Reina, Thomas Bose',
-    author_email='james.marshall@shef.ac.uk',
-    version='0.0',
-    url='https://github.com/DiODeProject/MuMoT',
-    packages=['MuMoT'],
-    package_dir={'':'.'},
-    license='GPL-3.0',
-    install_requires=requirements)
+      description='Multiscale Modelling Tool',
+      author='James A. R. Marshall, Andreagiovanni Reina, Thomas Bose',
+      author_email='james.marshall@shef.ac.uk',
+      # @todo: switch to using semantic versioning
+      # (https://packaging.python.org/tutorials/distributing-packages/#semantic-versioning-preferred)
+      version='0.0.0',
+      url='https://github.com/DiODeProject/MuMoT',
+      packages=['MuMoT'],
+      package_dir={'': '.'},
+      python_requires='>=3',
+      license='GPL-3.0',
+      install_requires=[
+          'graphviz',
+          'ipython',
+          'ipywidgets',
+          'matplotlib',
+          'networkx',
+          'pydstool',
+          'scipy<1.0.0',
+          'sympy>=1.1.1',
+          ],
+      )
