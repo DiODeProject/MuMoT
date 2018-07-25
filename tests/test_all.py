af774f512e9657f39f53ca0ceab57de82ed8bd83
Will Furnass <will@thearete.co.uk>
2018-07-25 11:07:49 +0100

    Add basic framework for unit testing (as mentioned in README)

---
+++ tests/test_all.py
@@ -0,0 +1,17 @@
+from mumot import MuMoTmodel
+
+
+def test_dummy_1():
+    """A brief description of this test.
+
+    More information about this test.
+    """
+    assert 1 == 1
+
+
+def test_dummy_2():
+    """A brief description of this test.
+
+    More information about this test.
+    """
+    assert 2 == 2
