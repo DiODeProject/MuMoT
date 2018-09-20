5ac513413e1735ca9fc2d280ba9272be5efe4b5c
Will Furnass <will@thearete.co.uk>
2018-09-20 16:15:10 +0100

    Pin sympy version to < 1.3 (Issue #170)

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'networkx',
         'pydstool',
         'scipy<1.0.0',  # see https://github.com/DiODeProject/MuMoT/issues/63
-        'sympy>=1.1.1',
+        'sympy >= 1.1.1, < 1.3'  # see https://github.com/DiODeProject/MuMoT/issues/170
         ],
     extras_require={
         'test': [
