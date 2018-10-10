56ede0e1ffa9f36a9d59e9ab5ab0eddf17c6c086
Will Furnass <will@thearete.co.uk>
2018-10-10 19:07:59 +0100

    Ensure can create models from a string instead of a cell reference

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,4 +1,9 @@
-from mumot import MuMoTmodel
+from textwrap import dedent
+
+from mumot import (
+    MuMoTmodel,
+    parseModel
+)
 
 
 def test_dummy_1():
@@ -15,3 +20,22 @@ def test_dummy_2():
     More information about this test.
     """
     assert 2 == 2
+
+
+def test_parse_model_from_cell_contents():
+    """Assert we can instantiate a MuMoTmodel from the contents of a Notebook cell that uses the %%model cell magic."""
+    s = "get_ipython().run_cell_magic('model', '', '$\\nU -> A : g_A\\nU -> B : g_B\\nA -> U : a_A\\nB -> U : a_B\\nA + U -> A + A : r_A\\nB + U -> B + B : r_B\\nA + B -> A + U : s\\nA + B -> B + U : s\\n$\\n')"
+    parseModel(s)
+
+
+def test_parse_model_from_str():
+    """Assert we can instantiate a MuMoTmodel from a multi-line string."""
+    s = dedent("""U -> A : g_A
+        U -> B : g_B
+        A -> U : a_A
+        B -> U : a_B
+        A + U -> A + A : r_A
+        B + U -> B + B : r_B
+        A + B -> A + U : s
+        A + B -> B + U : s""")
+    parseModel(s)
