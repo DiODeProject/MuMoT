DiODeProject/MuMoT

commit ff586d4a007f9990cb5a558c8c8dfd68e05c467b

Replaced the code that mutates keys when iterate over dicts with alternative solution. This should fix #379

mumot/views.py
@@ -1165,21 +1165,31 @@ class MuMoTnoiseCorrelationsView(MuMoTtimeEvolutionView):
                 display(Math(out))
         self._logs.append(log)
 
-        argDict = self._get_argDict()
-        for key in self._mumotModel._constantReactants:
-            if argDict[key] is not None:
-                argDict[sympy.Symbol(f"Phi_{key}")] = argDict.pop(key)
-        for key in steadyStateDict:
-            if key in self._mumotModel._reactants:
-                steadyStateDict[sympy.Symbol(f"Phi_{key}")] = steadyStateDict.pop(key)
+        argDict_tmp = self._get_argDict()
+#         for key in self._mumotModel._constantReactants:
+#             if argDict[key] is not None:
+#                 argDict[sympy.Symbol(f"Phi_{key}")] = argDict.pop(key)
+#         for key in steadyStateDict:
+#             if key in self._mumotModel._reactants:
+#                 steadyStateDict[sympy.Symbol(f"Phi_{key}")] = steadyStateDict.pop(key)
+        # Mutate key names of the argDict and the steadyStateDict so that they indicate concentrations via prefix Phi
+        argDict = {}
+        for key, val in argDict_tmp.items():
+            key_phi = sympy.Symbol(f"Phi_{key}") if key in self._mumotModel._constantReactants else: key
+            argDict[key_phi] = val
+        steadyStateDictPhi = {}
+        for key, val in steadyStateDict.items():
+            key_phi = sympy.Symbol(f"Phi_{key}") if key in self._mumotModel._reactants else: key
+            steadyStateDictPhi[key_phi] = val
+        
         EOM_1stOrdMomDict = copy.deepcopy(self._EOM_1stOrdMomDict)
         for sol in EOM_1stOrdMomDict:
-            EOM_1stOrdMomDict[sol] = EOM_1stOrdMomDict[sol].subs(steadyStateDict)
+            EOM_1stOrdMomDict[sol] = EOM_1stOrdMomDict[sol].subs(steadyStateDictPhi)
             EOM_1stOrdMomDict[sol] = EOM_1stOrdMomDict[sol].subs(argDict)
 
         EOM_2ndOrdMomDict = copy.deepcopy(self._EOM_2ndOrdMomDict)
 
-        SOL_2ndOrdMomDict = self._numericSol2ndOrdMoment(EOM_2ndOrdMomDict, steadyStateDict, argDict)
+        SOL_2ndOrdMomDict = self._numericSol2ndOrdMoment(EOM_2ndOrdMomDict, steadyStateDictPhi, argDict)
 
         time_depend_noise = []
         for reactant in self._mumotModel._reactants:
@@ -1689,10 +1699,12 @@ class MuMoTfieldView(MuMoTview):
                 M_2 = sympy.Function('M_2')
 
                 systemSize = sympy.Symbol('systemSize')
-                argDict = self._get_argDict()
-                for key in argDict:
-                    if key in self._mumotModel._constantReactants:
-                        argDict[sympy.Symbol('Phi_' + str(key))] = argDict.pop(key)
+                argDict_tmp = self._get_argDict()
+                # Mutate key names of argDict so that they indicate concentrations via prefix Phi
+                argDict = {}
+                for key, val in argDict_tmp.items():
+                    key_phi = sympy.Symbol(f"Phi_{key}") if key in self._mumotModel._constantReactants else: key
+                    argDict[key_phi] = val
 
                 realEQsol, eigList = self._get_fixedPoints2d()
 

mumot/models.py
@@ -114,17 +114,33 @@ class MuMoTmodel:
         for sub in subs:
             if sub[0] in newModel._reactants and len(sub[1].atoms(Symbol)) == 1:
                 raise exceptions.MuMoTSyntaxError(f"Using substitute to rename reactants not supported: {sub[0]} = {sub[1]}")
+#         for reaction in newModel._stoichiometry:
+#             for sub in subs:
+#                 newModel._stoichiometry[reaction]['rate'] = newModel._stoichiometry[reaction]['rate'].subs(sub[0], sub[1])
+#                 for reactant in newModel._stoichiometry[reaction]:
+#                     if not reactant == 'rate':
+#                         if reactant == sub[0]:
+#                             if '+' not in str(sub[1]) and '-' not in str(sub[1]):
+#                                 # replace keys according to: dictionary[new_key] = dictionary.pop(old_key)
+#                                 newModel._stoichiometry[reaction][sub[1]] = newModel._stoichiometry[reaction].pop(reactant)
+#                             else:
+#                                 newModel._stoichiometry[reaction][reactant].append({reactant: sub[1]})
+        # Creating a new stoichiometry dictionary for every reaction by substituting keys and values when necessary
         for reaction in newModel._stoichiometry:
             for sub in subs:
                 newModel._stoichiometry[reaction]['rate'] = newModel._stoichiometry[reaction]['rate'].subs(sub[0], sub[1])
-                for reactant in newModel._stoichiometry[reaction]:
-                    if not reactant == 'rate':
-                        if reactant == sub[0]:
-                            if '+' not in str(sub[1]) and '-' not in str(sub[1]):
-                                # replace keys according to: dictionary[new_key] = dictionary.pop(old_key)
-                                newModel._stoichiometry[reaction][sub[1]] = newModel._stoichiometry[reaction].pop(reactant)
-                            else:
-                                newModel._stoichiometry[reaction][reactant].append({reactant: sub[1]})
+                new_stoichiometry_dict = {}
+                for stoich_key, stoich_value in newModel._stoichiometry[reaction].items():
+                    new_st_key = stoich_key
+                    new_st_value = stoich_value
+                    if (not stoich_key == 'rate') and stoich_key == sub[0]: #  check if substitution is necessary 
+                        if '+' not in str(sub[1]) and '-' not in str(sub[1]): # substitute key
+                            new_st_key = sub[1]
+                        else: # substitute value
+                            new_st_value.append({stoich_key: sub[1]})
+                    new_stoichiometry_dict[new_st_key] = new_st_value
+                newModel._stoichiometry[reaction] = new_stoichiometry_dict
+
         for reactant in newModel._reactants:
             for sub in subs:
                 newModel._equations[reactant] = newModel._equations[reactant].subs(sub[0], sub[1])

commit de8757ff7dd8103ff6b6881b0dbecff9c4b52c04

corrected syntax bug introduced with commit https://github.com/DiODeProject/MuMoT/commit/ff586d4a007f9990cb5a558c8c8dfd68e05c467b

mumot/views.py
@@ -1175,11 +1175,11 @@ class MuMoTnoiseCorrelationsView(MuMoTtimeEvolutionView):
         # Mutate key names of the argDict and the steadyStateDict so that they indicate concentrations via prefix Phi
         argDict = {}
         for key, val in argDict_tmp.items():
-            key_phi = sympy.Symbol(f"Phi_{key}") if key in self._mumotModel._constantReactants else: key
+            key_phi = sympy.Symbol(f"Phi_{key}") if key in self._mumotModel._constantReactants else key
             argDict[key_phi] = val
         steadyStateDictPhi = {}
         for key, val in steadyStateDict.items():
-            key_phi = sympy.Symbol(f"Phi_{key}") if key in self._mumotModel._reactants else: key
+            key_phi = sympy.Symbol(f"Phi_{key}") if key in self._mumotModel._reactants else key
             steadyStateDictPhi[key_phi] = val
         
         EOM_1stOrdMomDict = copy.deepcopy(self._EOM_1stOrdMomDict)
@@ -1703,7 +1703,7 @@ class MuMoTfieldView(MuMoTview):
                 # Mutate key names of argDict so that they indicate concentrations via prefix Phi
                 argDict = {}
                 for key, val in argDict_tmp.items():
-                    key_phi = sympy.Symbol(f"Phi_{key}") if key in self._mumotModel._constantReactants else: key
+                    key_phi = sympy.Symbol(f"Phi_{key}") if key in self._mumotModel._constantReactants else key
                     argDict[key_phi] = val
 
                 realEQsol, eigList = self._get_fixedPoints2d()